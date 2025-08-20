923e5b6e83de5ae8d4a68ec84621e0723477cfbe
Daniil Oberlev <denioberlev@gmail.com>
2025-08-20 17:16:31 +0300

    chore(formatter): add base formatter

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  quoteProps: 'consistent'
+}
